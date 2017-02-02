3890919bf6e0f784c7aa56e2215a80c197ea026d
Xiao Gu <xiaogu@Admins-MacBook-Pro.local>
2017-02-02 13:37:04 -0800

    Added Bootstrap DataFrame methods

--- tools/bootstrapTTest.py
+++ tools/bootstrapTTest.py
@@ -8,6 +8,14 @@ def Resample(array):
 	sample = np.random.choice(array, N, replace=True)
 	return sample
 
+def ResampleDF(df):
+    N = len(df)
+    indexes = df.index
+    newIndex = Resample(indexes)
+    newDF = df.loc[newIndex,]
+    newDF.reset_index(drop=True, inplace=True)
+    return newDF
+
 def getMean(array):
 	return array.mean()
 
@@ -23,6 +31,6 @@ def WechTtest(arr1, arr2, B=400):
 	rearr1 = NPBootstrap(arr1, B)
 	rearr2 = NPBootstrap(arr2, B)
 	test = sp.stats.ttest_ind(rearr1, rearr2, equal_var=False)
-	return test
+	return rearr1, rearr2, test
 
-
+    
